58886b4f46f6bb97cdde74f67c2d581cfc7aac5f
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-02 23:46:48 +0700

    Add Feature and Style at Categories

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import './categories.css'
+import { React, useEffect, useState } from "react";
+import "./categories.css";
+import { filterListData } from "../data/filterListData";
+import GameCard from "../components/GameCard";
 
-function Categories({games, reference}) {
+function Categories({ games, reference }) {
+  const [filters, setFilters] = useState(filterListData);
+  const [data, setData] = useState(games)
+
+  useEffect(() => {
+    setData(games)
+  }, [games]);
+
+  const handleFilterGames = (category) => {
+    setFilters(
+      filters.map((filter) => {
+        filter.active = false;
+        if (filter.name === category) {
+          filter.active = true;
+        }
+        return filter;
+      })
+    );
+    if(category === 'All'){
+      return setData(games)
+    }else{
+      setData(games.filter(game => game.category === category))
+    }
+
+  };
+
+  const [text, setText] = useState('')
+
+  const handleSearchGames = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
-    <div className='categories' id='categpries' ref={reference}>
-        <h1>PPPPPPPPPPPPPPPPPPPP</h1>
-    </div>
-  )
+    <section className="categories" id="categories" ref={reference}>
+      <div className="container">
+        <div className="filters-and-search">
+          <ul className="filters">
+            {filters.map((filter) => (
+              <li
+                key={filter._id}
+                className={filter.active ? 'active' : ''}
+                onClick={() => handleFilterGames(filter.name)}
+              >
+                {filter.name}
+              </li>
+            ))}
+          </ul>
+          <div className="search">
+            <i className="bi bi-search"></i>
+            <input type="text" name="search" placeholder="Search" onChange={handleSearchGames}/>
+          </div>
+        </div>
+        <div className="row">
+            {data.map((game) => (
+              <GameCard key={game._id} game={game} />
+            ))}
+  
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Categories
+export default Categories;
