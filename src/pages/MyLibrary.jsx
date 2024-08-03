e561f06b43bf559c046e23e867407face5f561ad
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-04 02:08:29 +0700

    Create a useContext

--- src/pages/MyLibrary.jsx
+++ src/pages/MyLibrary.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import './mylibrary.css'
-import GameCard from '../components/GameCard'
+import React from "react";
+import "./mylibrary.css";
+import GameCard from "../components/GameCard";
 
-function MyLibrary({games, reference}) {
-  console.log(games)
+function MyLibrary({ games, reference }) {
+  console.log(games);
   return (
-    <section id='library' className='library' ref={reference}>
+    <section id="library" className="library" ref={reference}>
       <h1>My Library</h1>
       <div className="row">
-        {
-          games.length === 0 ? (
-            <h2>Your Library is empty</h2>
-          ) : games.map(game => (
-            <GameCard key={game._id} game={game}/>
-          ))
-        }
+        {games.length === 0 ? (
+          <h2>Your Library is empty</h2>
+        ) : (
+          games.map((game) => <GameCard key={game._id} game={game} />)
+        )}
       </div>
     </section>
-  )
+  );
 }
 
-export default MyLibrary
+export default MyLibrary;
